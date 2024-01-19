0ac6e40a06edb8bb4a4a92e8a6e2d24c9f1e661b
Zephyr <107991565+Doavahkiin@users.noreply.github.com>
2024-01-20 01:16:00 +0600

    Update express.js

--- events/express.js
+++ events/express.js
@@ -1,23 +1,8 @@
-const express = require("express");
-const app = express();
+const http = require('http');
 
-const server = app.listen(5615, () => {
-  console.log(" _______________________________");
-  console.log("");
-  console.log("| WEB-WINDOW POWERED BY EXPRESS |");
-  console.log(" _______________________________");
-  console.log("");
-  console.log("| /./\\//? Booting Client ...... |");
-  console.log("| Please be patient ........... |");
-  console.log(" _______________________________");
-  console.log("");
-  console.log("| Use \"kill 1\" in shell if stuck|");
-  console.log(" _______________________________");
-});
-
-app.get("/", (req, res) => {
-  res.setHeader("Content-Type", "text/html");
-  res.send(`
+const server = http.createServer((req, res) => {
+  res.setHeader('Content-Type', 'text/html');
+  res.end(`
     <html>
       <head>
         <title>Your Web View</title>
@@ -25,6 +10,9 @@ app.get("/", (req, res) => {
       <body style="margin: 0; padding: 0;">
         <iframe width="100%" height="100%" src="https://axocoder.vercel.app/" frameborder="0" allowfullscreen></iframe>
       </body>
-    </html>
-  `);
+    </html>`);
+});
+
+server.listen(3000, () => {
+  console.log('Server Online because of Axo Coder ✅!!');
 });
